dd8ee16d23c866869bcaa1efa7f9285845ff404d
MauroLGomez <piberandom777@gmail.com>
2023-04-16 16:07:47 -0300

    Updated and gave styles to the contact module

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Main from "./components/Main";
 import Projects from "./components/Projects";
 import Sidenav from "./components/sidenav";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <Main />
       <Work />
       <Projects />
+      <Contact />
     </div>
   )
 }
