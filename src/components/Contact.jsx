3bce4f1d39379e17019af35856212d952789f4ac
MauroLGomez <piberandom777@gmail.com>
2023-04-19 22:03:24 -0300

    grid change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,6 @@ const Contact = () => {
         Contact
         <form action="https://getform.io/f/bd60e0a6-aae7-4976-b3c4-a58496676d4b" method="POST" encType="multipart/form-data">
             <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                
-                </div>
                 <div className="flex flex-col py-2">
                     <label className="uppercase text-sm py-2">Name</label>
                     <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="name"/>
@@ -36,6 +34,7 @@ const Contact = () => {
                     <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
                     Send Message
                     </button>
+            </div>
         </form>
         </div>
     )
