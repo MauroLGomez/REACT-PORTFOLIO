f73978afff2bd24008e3be7ce9099b385da48797
MauroLGomez <piberandom777@gmail.com>
2023-04-19 21:10:26 -0300

    imgs new

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,18 +11,25 @@ const Projects = () => {
                 These are all the projects that i have been working on!
             </p>
             <div className="grid sm:grid-cols-2 gap-12">
-                <img src="https://imgur.com/CBWfpAn.png" title="React Calculator"/>
-                    <a href="https://github.com/MauroLGomez/calculadora"></a>
-                <img src="https://imgur.com/CBWfpAn.png" title="Landing Page"
+                <a href="https://github.com/MauroLGomez/calculadora">
+                    <img src="https://imgur.com/CBWfpAn.png" title="React Calculator"/>
+                </a>
+            
+                <a href="https://github.com/MauroLGomez/calculadora">
+                    <img src="https://imgur.com/CBWfpAn.png" title="Landing Page"
                 href="https://github.com/MauroLGomez/Prototipo-Pagina-Web/"/>
-                    <a href="https://github.com/MauroLGomez/calculadora"></a>
-                <img src="https://imgur.com/CBWfpAn.png" title="Portfolio React"
+                </a>
+                
+                <a href="https://github.com/MauroLGomez/calculadora">
+                    <img src="https://imgur.com/CBWfpAn.png" title="Portfolio React"
                 href="https://github.com/MauroLGomez/REACT-PORTFOLIO"/>
-                    <a href="https://github.com/MauroLGomez/calculadora"></a>
-                <img src="https://imgur.com/CBWfpAn.png" title="Qr Code"
-                href="https://github.com/MauroLGomez/qr-code-component"/>
-                    <a href="https://github.com/MauroLGomez/calculadora"></a>
-                <img src="../../assets/calculatorReact.png" alt="" />
+                </a>
+                
+                <a href="https://github.com/MauroLGomez/calculadora">
+                    <img src="https://imgur.com/CBWfpAn.png" title="Qr Code"
+                    href="https://github.com/MauroLGomez/qr-code-component"/>
+                </a>
+                
             </div>
         </div>
     );
