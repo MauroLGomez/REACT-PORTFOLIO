7d512263811e775694fff56803e9eebafeea2248
MauroLGomez <piberandom777@gmail.com>
2023-04-19 22:00:06 -0300

    a

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,23 +17,23 @@ const Contact = () => {
                     type="text" 
                     name="phone" />
                 </div>
-            </div>
-            <div className="flex flex-col py-2">
-                <label className="uppercase text-sm py-2">Email</label>
-                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email"/>
-            </div>
-            <div className="flex flex-col py-2">
-                <label className="uppercase text-sm py-2">Subject</label>
-                <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject"/>
-            </div>
-            <div className="flex flex-col py-2">
-                <label className="uppercase text-sm py-2">Message</label>
-                <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="10" name="message"> 
-                </textarea>
-            </div>
-            <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
-                Send Message
-            </button>
+                </div>
+                    <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2">Email</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email"/>
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2">Subject</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject"/>
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm py-2">Message</label>
+                    <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="10" name="message"> 
+                    </textarea>
+                </div>
+                    <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+                    Send Message
+                    </button>
         </form>
         </div>
     )
