3c19326de4059c02910fbb76757a9fb7e2cb59d0
MauroLGomez <piberandom777@gmail.com>
2023-04-19 21:19:26 -0300

    final commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,22 +11,20 @@ const Projects = () => {
                 These are all the projects that i have been working on!
             </p>
             <div className="grid sm:grid-cols-2 gap-12">
-                <a href="https://github.com/MauroLGomez/calculadora">
-                    <img src="https://imgur.com/CBWfpAn.png" title="React Calculator"/>
+                <a href="https://github.com/MauroLGomez/qr-code-component">
+                    <img src="https://i.imgur.com/CBWfpAn.png" title="Qr Code"/>
                 </a>
-            
-                <a href="https://github.com/MauroLGomez/calculadora">
-                    <img src="https://imgur.com/CBWfpAn.png" title="Landing Page"
-                href="https://github.com/MauroLGomez/Prototipo-Pagina-Web/"/>
+
+                <a href="https://github.com/MauroLGomez/Prototipo-Pagina-Web">
+                    <img src="https://i.imgur.com/6jMqr7f.png" title="Landing Page"/>
                 </a>
-                
-                <a href="https://github.com/MauroLGomez/calculadora">
-                    <img src="https://imgur.com/CBWfpAn.png" title="Portfolio React"
-                href="https://github.com/MauroLGomez/REACT-PORTFOLIO"/>
+
+                <a href="https://github.com/MauroLGomez/REACT-PORTFOLIO">
+                    <img src="https://i.imgur.com/MhKKJO3.jpg" title="React Portfolio"/>
                 </a>
                 
                 <a href="https://github.com/MauroLGomez/calculadora">
-                    <img src="https://imgur.com/CBWfpAn.png" title="Qr Code"/>
+                    <img src="https://i.imgur.com/huPdftA.png" title="Calculator React"/>
                 </a>
                 
             </div>
